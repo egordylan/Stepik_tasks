640839b46b0f3df813a46796e680b87ca937f758
Helen P <82153246+egordylan@users.noreply.github.com>
2021-09-30 12:54:29 +0200

    Add files via upload

---
+++ 15-2_guess_the_number.py
@@ -0,0 +1,52 @@
+"""
+Guess the number
+
+The program generates a random number in the range from 11 to 100100
+and asks a user to guess this number.
+If the user's guess is more than a random number then the program
+should display the message 'Too many, please try again'.
+If the guess is less than a random number, then the program should display
+'Too little, try again'.
+If the user guesses the number, then the program should congratulate him
+and display the message 'You guessed right, congratulations!'.
+"""
+
+import random
+
+print('The program generates a random number in the range from 11 to 100100.')
+
+
+def random_number():
+    #  this is a computer's number
+    random_num = random.randint(11, 100)
+    #  this is a user's number
+    num = int(input('Please, guess the number: ', ))
+    while True:
+        if num > random_num:
+            print('Too many, please try again.')
+            num = int(input('Please, guess the number: ',))
+        elif num < random_num:
+            print('Too little, try again.')
+            num = int(input('Please, guess the number: ',))
+        else:
+            print('You guessed right, congratulations!')
+            break
+
+    return num
+
+def play_again(answer):
+    if answer.lower() == 'y':
+        return random_number(), True
+    else:
+        print('Thank you for the game. Good bye.')
+        return False
+
+answer = True
+
+random_number()
+
+while answer:
+    print('Do you want to play again? (yes = y/ no = n)')
+    answer = play_again(input())
+
+
